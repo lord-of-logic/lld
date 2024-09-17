a8a0b24ece3a39063c6a1952a12058fb0cf7e6e4
Ranjith P <pranjith0869@gmail.com>
2024-09-17 10:52:59 +0530

    Update bridge_dp.kts

--- design_patterns/bridge_dp.kts
+++ design_patterns/bridge_dp.kts
@@ -5,37 +5,56 @@ interface UtensilType {
 
 //Concrete Implementor
 class Spoon: UtensilType {
-    var utensilName: String = "Spoon"
-    var utensilMaterial: String = "Steel"
+    private var utensilName: String = "Spoon"
+    private var utensilMaterial: String = "Steel"
     override fun doAction() {
-        println("tasted using spoon")
+        println("tasted using $utensilName made up of $utensilMaterial")
     }
 }
 
 class Plate: UtensilType {
-    var utensilName: String = "Plate"
-    var utensilMaterial: String = "Silver"
+    private var utensilName: String = "Plate"
+    private var utensilMaterial: String = "Silver"
     override fun doAction() {
-        println("ate from plate")
+        println("eaten using $utensilName made up of $utensilMaterial")
     }
 }
 
 class Glass: UtensilType {
-    var utensilName: String = "Glass"
-    var utensilMaterial: String = "Plastic"
+    private var utensilName: String = "Glass"
+    private var utensilMaterial: String = "Plastic"
     override fun doAction() {
-        println("drank from glass")
+        println("drunk using $utensilName made up of $utensilMaterial")
     }
 }
 
 //Abstracter
-abstract class FoodItem {
-    var utensilType: UtensilType? = null
-    
+abstract class FoodItem(var utensilType: UtensilType) {
+    abstract fun printFoodItemAndMode()
 }
 
 //Refined Abstracter
-class Water : FoodItem() {
-    var itemName: String = "Water"
-    var itemTaste: String = "None"
+class Water(utensilType: UtensilType) : FoodItem(utensilType) {
+    private var itemName: String = "Water"
+    private var itemTaste: String = "None"
+    override fun printFoodItemAndMode() {
+        println("$itemName was ")
+        utensilType.doAction()
+    }
+}
+
+class GulabJamun(utensilType: UtensilType): FoodItem(utensilType) {
+    private var itemName: String = "GulabJamun"
+    private var itemTaste: String = "Sweet"
+    override fun printFoodItemAndMode() {
+        println("$itemName was ")
+        utensilType.doAction()
+    }
+}
+
+fun main() {
+    val foodItem1 = Water(Glass())
+    foodItem1.printFoodItemAndMode()
+    val foodItem2 = GulabJamun(Spoon())
+    foodItem2.printFoodItemAndMode()
 }
