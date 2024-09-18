4630901aa3050d2484d234f6ca3cedc6cb3fc909
Ranjith P <pranjith0869@gmail.com>
2024-09-18 23:13:44 +0530

    Update strategy_dp.kts

--- design_patterns/behavioral/strategy_dp.kts
+++ design_patterns/behavioral/strategy_dp.kts
@@ -1,1 +1,42 @@
+//Strategy Interface
+interface DoSomethingStrategy {
+    fun doSomething()
+}
 
+//Strategy concrete implementation
+class FirstStrategy : DoSomethingStrategy {
+    override fun doSomething() {
+        println("First strategy")
+    }
+}
+
+class SecondStrategy : DoSomethingStrategy {
+    override fun doSomething() {
+        println("Second strategy")
+    }
+}
+
+//Strategy Context
+class DoSomethingContext {
+    private lateinit var doSomethingStrategy: DoSomethingStrategy
+
+    fun setDoSomethingStrategy(doSomethingStrategy: DoSomethingStrategy): DoSomethingStrategy {
+        this.doSomethingStrategy = doSomethingStrategy
+        return this.doSomethingStrategy
+    }
+
+    fun doSomething() {
+        doSomethingStrategy.doSomething()
+    }
+}
+
+//Client
+fun main() {
+    val inputStrategy = "First"
+    val strategyToExecute = 
+        if (inputStrategy == "First")
+            DoSomethingContext().setDoSomethingStrategy(FirstStrategy())
+        else
+            DoSomethingContext().setDoSomethingStrategy(SecondStrategy())
+    strategyToExecute.doSomething()
+}
